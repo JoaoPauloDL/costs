445d8b01979203636cfde24cec37f3b72122fbb9
João Paulo <joaopaulomayka@gmail.com>
2023-10-18 20:17:33 -0300

    feat: adding routes to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  )
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/company" element={<Company />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/newproject" element={<NewProject />}></Route>
+      </Routes>
+      <footer>Rodapé</footer>
+    </Router>
+  );
 }
 
 export default App;
