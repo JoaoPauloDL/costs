3a88a2e1150a3ce3f6b9755a0a5382fd8d9448a6
João Paulo <joaopaulomayka@gmail.com>
2023-10-19 15:50:10 -0300

    feat: Creation and configuration of the container component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/company" element={<Company />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/newproject" element={<NewProject />}></Route>
-      </Routes>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/company"}>Empresa</Link>
+        <Link to={"/contact"}>Contato</Link>
+        <Link to={"/newproject"}>Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/company" element={<Company />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
       <footer>Rodapé</footer>
     </Router>
   );
