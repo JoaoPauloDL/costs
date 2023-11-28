69d6015ed675c3510dfc6eceb42a2008a383545c
João Paulo <joaopaulomayka@gmail.com>
2023-11-28 15:42:42 -0300

    feat: addition of useState and useEffect hooks
    addition made to control the message display condition when the project is added

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -1,14 +1,32 @@
-import styles from './Message.module.css'
+import { useState, useEffect } from "react";
 
-function Message({type, msg}){
+import styles from "./Message.module.css";
 
-    return(
-        <div className={`${styles.message} ${styles[type]}`}>
-            {msg}           
-        </div>
-        
-        
-    )
+function Message({ type, msg }) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() =>{
+    if(!msg){
+        setVisible(false)
+        return
+    }
+
+    setVisible(true)
+
+    const timer = setTimeout(()=>{
+        setVisible(false)
+    }, 3000)
+
+
+  }, [msg])
+
+  return (
+    <>
+      {visible && (
+        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+      )}
+    </>
+  );
 }
 
-export default Message
+export default Message;
