6386cb043d5db1990c5f0a6fa4a1d461759b1749
João Paulo <joaopaulomayka@gmail.com>
2023-11-30 16:30:22 -0300

    feat: request in the api to search and display projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,34 +1,49 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
-import Message from "../layout/Message"
-import Container from '../layout/Container'
-import LinkButton from '../layout/LinkButton'
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
-import styles from './Projects.module.css'
+import styles from "./Projects.module.css";
+import ProjectCard from "../project/ProjectCard";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
 
-    const location = useLocation()
-    let message = ''
-    if(location.state) {
-        message = location.state.message
-    }
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
 
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <div className={styles.project_container}>
-            <div className={styles.title_container}>
-                <h1>Meus Projetos</h1>
-                <LinkButton to="/newproject" text="Criar Projeto"/>
-            </div>
-            
-            {message && <Message msg={message} type="success"/>}
-            <Container customClass="start">
-                <p>Projetos...</p>
-            </Container>
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
 
-        </div>
-    )
+      {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        <ProjectCard />
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
