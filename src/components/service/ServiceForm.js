8c53123b9bdd14de14a5fcc3f486ab8213e4b750
João Paulo <joaopaulomayka@gmail.com>
2023-12-07 21:41:35 -0300

    feat: Code for adding services

--- src/components/service/ServiceForm.js
+++ src/components/service/ServiceForm.js
@@ -5,44 +5,45 @@ import SubmitButton from "../form/SubmitButton";
 
 import styles from "../project/ProjectForm.module.css";
 
-function ServiceForm({handleSubmit, btnText, projectData}) {
+function ServiceForm({ handleSubmit, btnText, projectData }) {
 
-   function submit() {
+  const [service, setService] = useState({});
 
-   }    
-   
-   function handleChange (e) {
+  function submit(e) {
+    e.preventDefault()
+    projectData.services.push(service)
+    handleSubmit(projectData)
+  }
 
-   }
+  function handleChange(e) {
+    setService({...service, [e.target.name]: e.target.value})
+  }
 
   return (
-    <form onSubmit={submit}  className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
-        type = "text"
-        text = "Nome do serviço"
-        name = "name"
-        placeholder= "Insira o nome do serviço"
+        type="text"
+        text="Nome do serviço"
+        name="name"
+        placeholder="Insira o nome do serviço"
         handleOnChange={handleChange}
       />
       <Input
-        type = "number"
-        text = "Custo do serviço"
-        name = "cost"
-        placeholder= "Insira o valor total"
+        type="number"
+        text="Custo do serviço"
+        name="cost"
+        placeholder="Insira o valor total"
         handleOnChange={handleChange}
       />
-        <Input
-        type = "text"
-        text = "Descrição do serviço"
-        name = "description"
-        placeholder= "Descreva o serviço"
+      <Input
+        type="text"
+        text="Descrição do serviço"
+        name="description"
+        placeholder="Descreva o serviço"
         handleOnChange={handleChange}
       />
 
-      <SubmitButton text={btnText}/>
-
-
-
+      <SubmitButton text={btnText} />
     </form>
   );
 }
